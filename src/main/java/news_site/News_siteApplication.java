f42d330b17cb4e6451a634d1b0138bba5a1d371c
rubashkin_mikhail@tut.by <pristijpribor@mail.ru>
2020-11-18 23:15:33 +0300

    Initial commit

---
+++ src/main/java/news_site/News_siteApplication.java
@@ -0,0 +1,11 @@
+package news_site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class News_siteApplication {
+    public static void main (String[] args) {
+        SpringApplication.run(News_siteApplication.class, args);}
+}
---
+++ src/test/java/news_site/News_siteApplicationTests.java
@@ -0,0 +1,11 @@
+package news_site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class News_siteApplicationTests {
+    @Test
+    void contextLoads (){
+    }
+}
